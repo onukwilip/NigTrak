f67c30c0fe21c7d83d97cb7d5293debd1a354a92
memmcol-prince <memmcol.prince@gmail.com>
2023-02-08 16:40:20 +0100

    Begun and finished the Devices tab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Button, Divider, Icon, Input } from "semantic-ui-react";
 import Map from "../components/Map";
 import css from "../styles/home/Home.module.scss";
@@ -16,6 +16,9 @@ import cRank from "../assets/img/coloniel-rank.png";
 import lcRank from "../assets/img/lc-rank.png";
 import oRank from "../assets/img/officer-rank.png";
 import sRank from "../assets/img/sergent-rank.png";
+import walkieTalkieTrans from "../assets/img/walkie-talkie-trans.png";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Profile = () => {
   return (
@@ -78,9 +81,31 @@ const MapTab = ({ position }) => {
   );
 };
 
-const Card = ({ icon, className, header, value }) => {
+const Card = ({ icon, className, header, value, delay }) => {
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+  const variant = {
+    initial: { opacity: 0, x: -100 },
+    scaled: { opacity: 1, x: 0 },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      control.start("scaled");
+    } else {
+      control.start("initial");
+    }
+  }, [control, inView]);
+
   return (
-    <div className={`${css.card} ${className}`}>
+    <motion.div
+      variants={variant}
+      initial="initial"
+      animate={control}
+      className={`${css.card} ${className}`}
+      ref={ref}
+      transition={{ delay: delay / 30 }}
+    >
       <div className={css.body}>
         <i className={`${icon} ${css.icon}`} />
         <div className={css.desc}>
@@ -90,7 +115,7 @@ const Card = ({ icon, className, header, value }) => {
       </div>
 
       <i className={`fas fa-arrow-right-long ${css.next}`} />
-    </div>
+    </motion.div>
   );
 };
 
@@ -173,31 +198,37 @@ const Analytics = () => {
               icon="fas fa-walkie-talkie"
               header="Total devices distributed"
               value={10000}
+              delay={0}
             />
             <Card
               icon="fas fa-walkie-talkie"
               header="Total devices available"
               value={15000}
+              delay={1}
             />
             <Card
               icon="fas fa-walkie-talkie"
               header="Total devices remaining"
               value={5000}
+              delay={2}
             />
             <Card
               icon="fas fa-person-military-rifle"
               header="Total soldiers available"
               value={30000}
+              delay={3}
             />
             <Card
               icon="fas fa-person-military-rifle"
               header="Total soldiers with device"
               value={5000}
+              delay={4}
             />
             <Card
               icon="fa-solid fa-building-circle-exclamation"
               header="Total stations"
               value={200}
+              delay={5}
             />
           </div>
           <div className={css["doughnut-chart"]}>
@@ -222,9 +253,33 @@ const ranks = {
   Sergent: sRank,
 };
 
-const UserCard = ({ user, onViewMore = () => {} }) => {
+const UserCard = ({ user, onViewMore = () => {}, index }) => {
+  const [ref, inView] = useInView();
+  const control = useAnimation();
+  const variants = {
+    far: { opacity: 0, x: "100px" },
+    current: { opacity: 1, x: "0px" },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      control.start("current");
+    } else {
+      control.start("far");
+    }
+  }, [control, inView]);
+
   return (
-    <div className={css["user-card"]}>
+    <motion.div
+      variants={variants}
+      initial="far"
+      animate={control}
+      transition={{
+        delay: index / 50,
+      }}
+      className={css["user-card"]}
+      ref={ref}
+    >
       <div className={css["img-container"]}>
         <img src={user?.image} alt="" />
       </div>
@@ -244,15 +299,87 @@ const UserCard = ({ user, onViewMore = () => {} }) => {
           View more
         </Button>
       </div>
-    </div>
+    </motion.div>
+  );
+};
+
+const DeviceCard = ({ device, onViewMore = () => {}, index }) => {
+  const [ref, inView] = useInView();
+  const control = useAnimation();
+  const variants = {
+    far: { opacity: 0, x: "100px" },
+    current: { opacity: 1, x: "0px" },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      control.start("current");
+    } else {
+      control.start("far");
+    }
+  }, [control, inView]);
+
+  return (
+    <motion.div
+      variants={variants}
+      initial="far"
+      animate={control}
+      transition={{
+        delay: index / 50,
+      }}
+      className={css["device-card"]}
+      ref={ref}
+    >
+      <div className={css["img-container"]}>
+        <img src={walkieTalkieTrans} alt="" />
+      </div>
+      <div className={css.details}>
+        <em>
+          {device?._id?.substring(0, 5)}...
+          {device?._id?.substring(device?._id?.length, 21)}
+        </em>
+        <em>{device?.deviceHolderName}</em>
+      </div>
+      <div className={css.actions}>
+        <Button
+          onClick={() => {
+            onViewMore(device);
+          }}
+        >
+          View more
+        </Button>
+      </div>
+    </motion.div>
   );
 };
 
 const UsersList = ({ users, onViewMore, className }) => {
   return (
     <div className={`${css["users-list"]} ${className}`}>
-      {users.map((user) => (
-        <UserCard user={user} onViewMore={onViewMore} ranks={ranks} />
+      {users.map((user, i) => (
+        <UserCard
+          user={user}
+          onViewMore={onViewMore}
+          key={user._id}
+          ranks={ranks}
+          index={i}
+        />
+      ))}
+    </div>
+  );
+};
+
+const DevicesList = ({ devices, onViewMore, className }) => {
+  return (
+    <div className={`${css["users-list"]} ${className}`}>
+      {devices.map((device, i) => (
+        <DeviceCard
+          device={device}
+          onViewMore={onViewMore}
+          key={device._id}
+          ranks={ranks}
+          index={i}
+        />
       ))}
     </div>
   );
@@ -352,7 +479,77 @@ const Soldiers = ({ position }) => {
 };
 
 const Devices = () => {
-  return <div>Devices</div>;
+  const [device, setDevice] = useState(data.devices[0]);
+  const [devices, setDevices] = useState(data.devices);
+
+  const onSearch = (/**@type String */ value) => {
+    const filtered = data.devices.filter(
+      (eachDevice) =>
+        eachDevice._id.toLowerCase().includes(value.toLowerCase()) ||
+        eachDevice.deviceHolderName.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setDevices(filtered);
+  };
+
+  return (
+    <section className={css.devices}>
+      <div className={css.left}>
+        <div className={css["device-details"]}>
+          <div className={css.details}>
+            <ul>
+              <li>
+                <em>ID</em>: <em>{device["_id"]}</em>
+              </li>
+              <li>
+                <em>Registered</em>: <em>{device["registered"]}</em>
+              </li>{" "}
+              <li>
+                <em>Location</em>:{" "}
+                <em>
+                  {device["location"]["lat"]} {device["location"]["lng"]}
+                </em>
+              </li>{" "}
+              <li>
+                <em>State</em>: <em>{device["state"]}</em>
+              </li>
+              <li>
+                <em>Station</em>: <em>{device["station"]}</em>
+              </li>
+              <li>
+                <em>Militant ID</em>: <em>{device["deviceHolderId"]}</em>
+              </li>
+              <li>
+                <em>Militant name</em>: <em>{device["deviceHolderName"]}</em>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={css["map-container"]}>
+          {device["location"] && (
+            <Map newCenter={device["location"]} zoom={10} />
+          )}
+        </div>
+      </div>
+      <div className={css.right}>
+        <div className={css["search-container"]}>
+          <Input
+            className={css.input}
+            label="Search"
+            placeholder="Search devices"
+            onChange={(e) => {
+              onSearch(e.target.value);
+            }}
+          />
+        </div>
+        <DevicesList
+          devices={devices}
+          onViewMore={setDevice}
+          className={css.users}
+        />
+      </div>
+    </section>
+  );
 };
 
 const Stations = () => {
@@ -367,6 +564,7 @@ const Home = () => {
   const [position, setPosition] = useState(null);
   const [showProfile, setShowProfile] = useState(true);
   const [showMenu, setShowMenu] = useState(true);
+  const profileRef = useRef();
   const onSuccess = (pos) => {
     const crd = pos.coords;
 
@@ -378,6 +576,14 @@ const Home = () => {
     navigator.geolocation.getCurrentPosition(onSuccess);
     return () => {};
   }, []);
+
+  useEffect(() => {
+    if (window.innerWidth <= 450) {
+      setShowProfile(false);
+    } else {
+      setShowProfile(true);
+    }
+  }, [window.innerWidth]);
 
   return (
     <section className={css.home}>
@@ -458,6 +664,7 @@ const Home = () => {
             appearActive: css["profile-animate-appear-active"],
             appearDone: css["profile-animate-appear-done"],
           }}
+          ref={profileRef}
         >
           <Profile />
         </CSSTransition>
